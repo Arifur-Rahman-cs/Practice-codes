82f8572f57cd84d6258b1e1cc117f7044750261c
Arifur-Rahman-cs <68846755+Arifur-Rahman-cs@users.noreply.github.com>
2020-08-11 20:36:55 +0600

    Create Romans.py
    It was suppose to print "X".

---
+++ Romans.py
@@ -0,0 +1,14 @@
+romans = {
+    1 : "I", 2: "II", 3: "III", 4: "IV", 5: "V",
+    6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",
+
+}
+
+x = 10
+number = ""
+
+if x > 10:
+    r = x / 10
+    number = romans[10] * r
+
+print(number)
