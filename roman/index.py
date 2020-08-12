4ccb982ad1e6f40d63a46c99ce283e8464bfbb92
Arifur-Rahman-cs <68846755+Arifur-Rahman-cs@users.noreply.github.com>
2020-08-12 16:15:25 +0600

    Open the index file

---
+++ roman/index.py
@@ -0,0 +1,6 @@
+from romans import convert
+
+#This program converts integers from 1-60 into roman numericals
+# To call the function, call convert.int_to_roman(x)
+
+print(convert.int_to_roman(50))
